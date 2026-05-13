03fc084dd506537097641c71c600ddca32fa102e
Luan Alves <devluanpaiva@gmail.com>
2026-05-13 14:25:43 -0300

    feat: creating spring boot api

---
+++ api/observador-de-precos-api/src/main/java/com/luanpaiva/observador_de_precos_api/ObservadorDePrecosApiApplication.java
@@ -0,0 +1,13 @@
+package com.luanpaiva.observador_de_precos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservadorDePrecosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservadorDePrecosApiApplication.class, args);
+	}
+
+}
---
+++ api/observador-de-precos-api/src/test/java/com/luanpaiva/observador_de_precos_api/ObservadorDePrecosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luanpaiva.observador_de_precos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservadorDePrecosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
