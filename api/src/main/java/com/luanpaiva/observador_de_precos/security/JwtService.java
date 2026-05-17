12627017fcc6b4147176d5d28ba5fe694d6f28c0
Luan Alves <devluanpaiva@gmail.com>
2026-05-17 16:43:56 -0300

    feat(api): creating Jwt service

---
+++ api/src/main/java/com/luanpaiva/observador_de_precos/security/JwtService.java
@@ -0,0 +1,39 @@
+package com.luanpaiva.observador_de_precos.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+public class JwtService {
+    @Value("${security.jwt.secret}")
+    private String jwtSecret;
+
+    private SecretKey getSignInKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+
+    public String generateAccessToken(UUID userId) {
+        return Jwts.builder()
+                .subject(userId.toString())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    public UUID extractUserId(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return UUID.fromString(claims.getSubject());
+    }
+}
