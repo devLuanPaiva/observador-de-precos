f63ecef2f7f352027bc6db31aa8499c2fb73bd85
Luan Alves <devluanpaiva@gmail.com>
2026-05-17 15:14:11 -0300

    feat(api): creating User entity class

---
+++ api/src/main/java/com/luanpaiva/observador_de_precos/modules/auth/entity/User.java
@@ -0,0 +1,32 @@
+package com.luanpaiva.observador_de_precos.modules.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 120)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 180)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
