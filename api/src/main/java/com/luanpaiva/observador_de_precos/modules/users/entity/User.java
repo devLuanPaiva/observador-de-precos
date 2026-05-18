ccda7c39d56545576473e8eb2afd5f1180a06bd5
Luan Alves <devluanpaiva@gmail.com>
2026-05-18 16:24:06 -0300

    feat(api): Updating the User interface to implement UserDetails.

--- api/src/main/java/com/luanpaiva/observador_de_precos/modules/users/entity/User.java
+++ api/src/main/java/com/luanpaiva/observador_de_precos/modules/users/entity/User.java
@@ -4,7 +4,12 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "users")
@@ -13,7 +18,7 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
@@ -29,4 +34,14 @@ public class User {
 
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
