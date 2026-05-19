50276b0de596051ff2425f5b87cb495f2a0c42db
Luan Alves <devluanpaiva@gmail.com>
2026-05-19 08:23:35 -0300

    feat(api): protecting the user's response through the DTO

--- api/src/main/java/com/luanpaiva/observador_de_precos/modules/users/controller/UserController.java
+++ api/src/main/java/com/luanpaiva/observador_de_precos/modules/users/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.luanpaiva.observador_de_precos.modules.users.controller;
 
+import com.luanpaiva.observador_de_precos.modules.users.dto.UserResponseDTO;
 import com.luanpaiva.observador_de_precos.modules.users.entity.User;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -7,9 +8,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
     @GetMapping("/me")
-    public User me(
+    public UserResponseDTO me(
             @AuthenticationPrincipal User user) {
-        return user;
+
+        return new UserResponseDTO(
+                user.getId(),
+                user.getName(),
+                user.getEmail());
     }
 }
