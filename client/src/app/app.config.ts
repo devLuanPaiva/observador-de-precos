317589c5c483d79390531226baacd2d5655bcfc8
Luan Alves <devluanpaiva@gmail.com>
2026-05-19 10:19:44 -0300

    chore(client): registering the interceptor in the application

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,16 +1,22 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { authInterceptor } from '@features/auth/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([
+        authInterceptor
+      ])
+    ),
     provideClientHydration(withEventReplay()),
     provideStore(),
     provideEffects(),
