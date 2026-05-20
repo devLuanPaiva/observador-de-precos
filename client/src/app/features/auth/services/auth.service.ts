00857c622c4c0c87526da71ff7ff9510fe43ca2d
Luan Alves <devluanpaiva@gmail.com>
2026-05-20 14:13:21 -0300

    fix(client): initializing user Token signal with null.

--- client/src/app/features/auth/services/auth.service.ts
+++ client/src/app/features/auth/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { LoginRequest } from '../models/login-request.model';
 import { AuthResponse } from '../models/auth-response.model';
 import { tap } from 'rxjs';
@@ -9,11 +10,19 @@ import { tap } from 'rxjs';
 })
 export class AuthService {
   private readonly http = inject(HttpClient);
+  private readonly platformId = inject(PLATFORM_ID);
   apiUrl = signal(api_url)
 
-  userToken = signal<string | null>(
-    localStorage.getItem('userToken')
-  )
+  userToken = signal<string | null>(null)
+
+  constructor() {
+    if (isPlatformBrowser(this.platformId)) {
+      const token = localStorage.getItem('userToken');
+      if (token) {
+        this.userToken.set(token);
+      }
+    }
+  }
 
   login(payload: LoginRequest) {
     return this.http.post<AuthResponse>(
@@ -21,15 +30,18 @@ export class AuthService {
       payload
     ).pipe(
       tap(response => {
-        localStorage.setItem('userToken', response.accessToken);
-
+        if (typeof localStorage !== 'undefined') {
+          localStorage.setItem('userToken', response.accessToken);
+        }
         this.userToken.set(response.accessToken);
       })
     )
   }
 
   logout() {
-    localStorage.removeItem('userToken');
+    if (typeof localStorage !== 'undefined') {
+      localStorage.removeItem('userToken');
+    }
     this.userToken.set(null);
   }
 
