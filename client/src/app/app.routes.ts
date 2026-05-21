f9d0cc4e5633c156ae8bf2e80e6b0019d41fed77
Luan Alves <devluanpaiva@gmail.com>
2026-05-21 10:22:38 -0300

    chore(client): setting app routers

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from '@features/auth/layouts/auth-layout/auth-layout';
+import { LoginPage } from '@features/auth/pages/login-page/login-page';
+import { RegisterPage } from '@features/auth/pages/register-page/register-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AuthLayout,
+        children: [
+            {
+                path: 'login',
+                component: LoginPage
+            },
+            {
+                path: 'registrar-usuario',
+                component: RegisterPage
+            }
+        ]
+    }
+];
