19d704992bac345fd4499a467e941a04ef868baf
Luan Alves <devluanpaiva@gmail.com>
2026-05-22 10:30:41 -0300

    feat(client): Migrating the login form to a modern version using signalsForm

--- client/src/app/features/auth/pages/login-page/login-page.ts
+++ client/src/app/features/auth/pages/login-page/login-page.ts
@@ -1,63 +1,91 @@
-import { Component, inject, signal } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, computed, inject, signal } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthForm } from '@features/auth/components/auth-form/auth-form';
+
 import { AuthService } from '@features/auth/services/auth.service';
 import { finalize } from 'rxjs';
 
+import { form, FormField, required, email } from '@angular/forms/signals';
+
 @Component({
   selector: 'app-login-page',
-  imports: [AuthForm, ReactiveFormsModule],
+  imports: [ ReactiveFormsModule, FormField],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
 export class LoginPage {
-  private readonly fb = inject(FormBuilder);
   private readonly authService = inject(AuthService);
   private readonly router = inject(Router);
 
-  loading = signal(false);
+  isSubmitting = signal(false);
+  submitError = signal<string | null>(null);
+  submitSuccess = signal(false);
+  submitDisabled = signal(false);
 
-  errorMessage = signal<string | null>(null);
+  authModel = signal({
+    email: '',
+    password: '',
+  })
 
-  form = this.fb.nonNullable.group({
-    email: [
-      '',
-      [
-        Validators.required,
-        Validators.email,
-      ]
-    ],
-    password: [
-      '',
-      [
-        Validators.required,
-        Validators.minLength(6),
-      ]
-    ]
+  authForm = form(
+    this.authModel,
+    (schema) => {
+      required(schema.email, {
+        message: 'O email é obrigatório.'
+      });
+
+      email(schema.email, {
+        message: 'O email deve ser válido.'
+      })
+
+      required(schema.password, {
+        message: 'A senha é obrigatória.'
+      });
+
+    }
+  )
+
+
+  canSubmit = computed(() => {
+    return this.authForm().valid();
   })
 
   onSubmit() {
-    this.loading.set(true);
-    this.errorMessage.set(null);
+    if (!this.canSubmit()) {
+      this.authForm().markAsTouched();
+      return;
+    }
+
+    this.isSubmitting.set(true);
+    this.submitError.set(null);
+    this.submitDisabled.set(true);
+    this.submitSuccess.set(false);
 
     this.authService.login(
-      this.form.getRawValue()
+      this.authModel()
     ).pipe(
       finalize(() => {
-        this.loading.set(false);
+        this.isSubmitting.set(false);
+        this.submitDisabled.set(false);
+        
+        this.authModel.set({
+          email: '',
+          password: '',
+        })
+        
+        this.authForm().reset();
       })
     ).subscribe({
       next: () => {
+        this.submitSuccess.set(true);
         this.router.navigate(['/dashboard']);
       },
 
       error: () => {
-        this.errorMessage.set(
+        this.submitError.set(
           'Credenciais inválidas. Por favor, verifique seu email e senha e tente novamente.'
         )
       }
     })
   }
-
 }
