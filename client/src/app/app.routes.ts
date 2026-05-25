120b9635b6e5a1c3221dc1fb9442d18f6a52a4e5
Luan Alves <devluanpaiva@gmail.com>
2026-05-25 10:29:46 -0300

    feat(client): adding new routes to dashboard page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
+import { authGuard } from '@features/auth/guards/auth.guard';
 import { AuthLayout } from '@features/auth/layouts/auth-layout/auth-layout';
 import { LoginPage } from '@features/auth/pages/login-page/login-page';
 import { RegisterPage } from '@features/auth/pages/register-page/register-page';
+import { DashboardPage } from '@features/dashboard/pages/dashboard-page/dashboard-page';
+import { AppShellLayout } from '@layouts/app-shell/app-shell-layout/app-shell-layout';
 
 export const routes: Routes = [
     {
@@ -15,6 +18,20 @@ export const routes: Routes = [
             {
                 path: 'registrar-usuario',
                 component: RegisterPage
+            }
+        ]
+    },
+    {
+        path: '',
+        component: AppShellLayout,
+        canActivate: [
+            authGuard
+        ],
+        children: [
+
+            {
+                path: 'dashboard',
+                component: DashboardPage
             }
         ]
     }
