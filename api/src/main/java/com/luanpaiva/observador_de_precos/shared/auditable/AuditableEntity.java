2e594ee30d1ff4f4ea27c9bc87dadeef1d3bca7a
Luan Alves <devluanpaiva@gmail.com>
2026-06-08 15:14:56 -0300

    feat(api): ensuring that Spring Data JPA auditing is actually working in production.

--- api/src/main/java/com/luanpaiva/observador_de_precos/shared/auditable/AuditableEntity.java
+++ api/src/main/java/com/luanpaiva/observador_de_precos/shared/auditable/AuditableEntity.java
@@ -5,16 +5,23 @@ import java.util.UUID;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import lombok.experimental.SuperBuilder;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
+@SuperBuilder
+@NoArgsConstructor
 public abstract class AuditableEntity {
-
     @CreatedDate
     private LocalDateTime createdAt;
 
@@ -22,6 +29,5 @@ public abstract class AuditableEntity {
     private LocalDateTime updatedAt;
 
     private UUID createdBy;
-
     private UUID updatedBy;
 }
