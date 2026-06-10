f42903911c817bb4b7970e01fb1e376e6d1ffe66
Luan Alves <devluanpaiva@gmail.com>
2026-06-10 15:54:40 -0300

    feat(client): creating loading service

---
+++ client/src/app/core/loading/loading.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, signal } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LoadingService {
+    private readonly loadingSignal = signal(false);
+
+    readonly loading =
+        this.loadingSignal.asReadonly();
+
+    show() {
+        this.loadingSignal.set(true);
+    }
+
+    hide() {
+        this.loadingSignal.set(false);
+    }
+}
