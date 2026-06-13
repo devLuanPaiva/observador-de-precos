6ed1fbd86055ee0835ee822bff80131da2dfcbda
Luan Alves <devluanpaiva@gmail.com>
2026-06-13 11:47:24 -0300

    refactor(api): returning role field in the user response dto

--- api/src/main/java/com/luanpaiva/observador_de_precos/modules/users/dto/UserResponseDTO.java
+++ api/src/main/java/com/luanpaiva/observador_de_precos/modules/users/dto/UserResponseDTO.java
@@ -2,8 +2,11 @@ package com.luanpaiva.observador_de_precos.modules.users.dto;
 
 import java.util.UUID;
 
+import com.luanpaiva.observador_de_precos.modules.users.enums.UserRole;
+
 public record UserResponseDTO(
-        UUID id,
-        String name,
-        String email) {
+                UUID id,
+                String name,
+                String email,
+                UserRole role) {
 }
