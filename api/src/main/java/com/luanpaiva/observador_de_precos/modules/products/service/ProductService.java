77c9872f0c0ca149387b1bdc515f036d949e465c
Luan Alves <devluanpaiva@gmail.com>
2026-06-16 10:04:36 -0300

    feat(api): implementing ProductService interface

---
+++ api/src/main/java/com/luanpaiva/observador_de_precos/modules/products/service/ProductService.java
@@ -0,0 +1,29 @@
+package com.luanpaiva.observador_de_precos.modules.products.service;
+
+import java.util.UUID;
+
+import com.luanpaiva.observador_de_precos.modules.products.dto.CreateProductRequestDTO;
+import com.luanpaiva.observador_de_precos.modules.products.dto.ProductResponseDTO;
+import com.luanpaiva.observador_de_precos.modules.products.dto.UpdateProductRequestDTO;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ProductService {
+    ProductResponseDTO createProduct(
+            CreateProductRequestDTO createProductRequestDTO);
+
+    ProductResponseDTO update(
+            UUID id,
+            UpdateProductRequestDTO dto);
+
+    ProductResponseDTO findById(
+            UUID id);
+
+    Page<ProductResponseDTO> findAll(
+            String search,
+            Pageable pageable);
+
+    void delete(
+            UUID id);
+}
