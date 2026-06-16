a46d13ab1b7c2f29c7c878c107be70a7dd026020
Luan Alves <devluanpaiva@gmail.com>
2026-06-16 15:54:08 -0300

    feat(api): adding user in the product table

--- api/src/main/java/com/luanpaiva/observador_de_precos/modules/products/entity/Product.java
+++ api/src/main/java/com/luanpaiva/observador_de_precos/modules/products/entity/Product.java
@@ -3,13 +3,17 @@ package com.luanpaiva.observador_de_precos.modules.products.entity;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import com.luanpaiva.observador_de_precos.modules.users.entity.User;
 import com.luanpaiva.observador_de_precos.shared.auditable.AuditableEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -46,6 +50,10 @@ public class Product extends AuditableEntity {
 
     @Column
     private Boolean active;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @Column(name = "current_price", precision = 10, scale = 2)
     private BigDecimal currentPrice;
