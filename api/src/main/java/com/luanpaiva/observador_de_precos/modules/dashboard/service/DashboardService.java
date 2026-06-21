d16087b0ff832afe4d18ff4d0a057a4200c73f72
Luan Alves <devluanpaiva@gmail.com>
2026-06-21 15:43:20 -0300

    feat(api): creating DashboardService interface

---
+++ api/src/main/java/com/luanpaiva/observador_de_precos/modules/dashboard/service/DashboardService.java
@@ -0,0 +1,8 @@
+package com.luanpaiva.observador_de_precos.modules.dashboard.service;
+
+import com.luanpaiva.observador_de_precos.modules.dashboard.dto.DashboardResponseDTO;
+
+public interface DashboardService {
+
+    DashboardResponseDTO getDashboard();
+}
