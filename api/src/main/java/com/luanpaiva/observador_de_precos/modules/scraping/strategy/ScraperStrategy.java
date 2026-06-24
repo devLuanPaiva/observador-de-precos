5af5642b6fb8efbf673b97c60cf79ea645bbadf2
Luan Alves <devluanpaiva@gmail.com>
2026-06-24 16:32:56 -0300

    feat(api): adding new field in the scrapper strategy interface

--- api/src/main/java/com/luanpaiva/observador_de_precos/modules/scraping/strategy/ScraperStrategy.java
+++ api/src/main/java/com/luanpaiva/observador_de_precos/modules/scraping/strategy/ScraperStrategy.java
@@ -4,5 +4,7 @@ import com.luanpaiva.observador_de_precos.modules.scraping.dto.ScrapingResultDTO
 
 public interface ScraperStrategy {
 
+    boolean supports(String url);
+
     ScrapingResultDTO scrape(String url);
 }
