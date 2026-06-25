ecf88cc8f34a82ea4e683781584a2e387f709ddc
Luan Alves <devluanpaiva@gmail.com>
2026-06-25 11:09:24 -0300

    chore(api): enaling scheduler in the main application

--- api/src/main/java/com/luanpaiva/observador_de_precos/ObservadorDePrecosApplication.java
+++ api/src/main/java/com/luanpaiva/observador_de_precos/ObservadorDePrecosApplication.java
@@ -2,7 +2,9 @@ package com.luanpaiva.observador_de_precos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ObservadorDePrecosApplication {
 
